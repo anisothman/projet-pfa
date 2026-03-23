28ca9eaa02fb1c64cfc64b8721d5c9a90f6467f2
israwerghemmi <isra.werghemmi@isimm.u-monastir.tn>
2026-03-23 11:59:19 +0100

    correction path and import

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import json
-from serp_client import rechercher
-from json_extractor import JSONExtractor
+from src.serp_client import rechercher
+from src.json_extractor import JSONExtractor
 
 
 def rechercher_et_extraire(nom_entreprise: str) -> None:
